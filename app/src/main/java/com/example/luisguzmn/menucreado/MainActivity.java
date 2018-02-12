04499c8dd7e631a556c3a579e52008fb0c2516f8
luis95gr <36051490+luis95gr@users.noreply.github.com>
2018-02-12 00:40:25 -0600

    Hoa

--- app/src/main/java/com/example/luisguzmn/menucreado/MainActivity.java
+++ app/src/main/java/com/example/luisguzmn/menucreado/MainActivity.java
@@ -1,15 +1,17 @@
 package com.example.luisguzmn.menucreado;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
@@ -34,21 +36,33 @@ public class MainActivity extends AppCompatActivity {
         if(mToggle.onOptionsItemSelected(item)){
             return true;
         }
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.actions) {
+            return true;
+        }
         return super.onOptionsItemSelected(item);
 
     }
 
 
+
+
+
+
+    @Override
     @SuppressWarnings("StatementWithEmptyBody")
     public boolean onNavigationItemSelected(MenuItem item){
 
         int id = item.getItemId();
 
-
         if (id == R.id.main){
+            Intent intent = new Intent(MainActivity.this, Pantalla2.class);
+            startActivity(intent);
 
         }else if(id == R.id.profile){
-            //fragmentManager.beginTransaction().replace(R.id.contenedor,new profileFragment()).commit();
             Intent intent = new Intent(MainActivity.this, Pantalla2.class);
             startActivity(intent);
         }else if(id == R.id.statistics){
